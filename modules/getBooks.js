4dde929f18c6d2d98463b24cdad58e1e9f01921f
DrewStroede <dstrow13@gmail.com>
2023-12-20 01:26:40 -0600

    added components for scurity

--- modules/getBooks.js
+++ modules/getBooks.js
@@ -3,7 +3,9 @@
 const Book = require('../books');
 
 async function getBooks(req, res, next){
-    const books= await Book.find({})
+    const userEmail = req.user.email;
+    console.log('Here is the user: ', userEmail)
+    const books= await Book.find({user: userEmail})
     res.status(200).send(books)
     // .then(data => res.status(200).send(data))
     // .catch(e => next(e));
